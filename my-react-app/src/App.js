bc654d3420ae901eaaf9e657c413a22b3f66def7
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2024-06-08 02:22:44 +0000

    ajout

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,7 +1,13 @@
+import UserList from "./UserList";
+
 function App() {
-  return (
-    <div>App</div>
-  )
-}
+
+    return (
+        <div style={{backgroundColor: 'white', width: '300px', margin: '100px auto', border: '1px solid rgba(0, 0, 0, 0.4)'}}>
+            <h3 style={{textAlign: 'center', textDecoration: 'underline'}}>LISTE DES UTILISATEURS</h3>
+            <UserList />
+        </div>
+    );
+};
 
 export default App
